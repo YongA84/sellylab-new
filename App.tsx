3637b0e6dd93893c4a750345fcf79a92f38fb9b9
YongA84 <cbelnom@gmail.com>
2026-04-19 20:50:31 +0900

    fix SafeAreaProvider crash

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
+    <>
       <StatusBar style="light" backgroundColor="#08090d" />
       <RootNavigator />
-    </SafeAreaProvider>
+    </>
   );
 }
